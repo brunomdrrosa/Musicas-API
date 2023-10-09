799d5f61961668e5f3b55b95128466a593f16d29
Bruno Machado <brunomdr46@gmail.com>
2023-10-08 22:15:24 -0300

    Correção de CORS

--- src/main/java/br/com/bruno/musicas_a_p_i/MusicasAPIApplication.java
+++ src/main/java/br/com/bruno/musicas_a_p_i/MusicasAPIApplication.java
@@ -1,9 +1,11 @@
 package br.com.bruno.musicas_a_p_i;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class MusicasAPIApplication {
 
