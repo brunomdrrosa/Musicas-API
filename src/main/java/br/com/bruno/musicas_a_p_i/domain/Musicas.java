768aa42982ef93b167b93d41f01cf66e1b8cc71d
Bruno Machado <brunomdr46@gmail.com>
2023-10-08 21:17:38 -0300

    Ajuste na entidade

--- src/main/java/br/com/bruno/musicas_a_p_i/domain/Musicas.java
+++ src/main/java/br/com/bruno/musicas_a_p_i/domain/Musicas.java
@@ -12,17 +12,8 @@ import jakarta.persistence.SequenceGenerator;
 public class Musicas {
 
     @Id
-    @Column(nullable = false, updatable = false)
-    @SequenceGenerator(
-            name = "primary_sequence",
-            sequenceName = "primary_sequence",
-            allocationSize = 1,
-            initialValue = 10000
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "primary_sequence"
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(nullable = false)
